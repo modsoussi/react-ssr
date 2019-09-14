fab123f37e0e521901f6fd7fdd0ed691f5a7a10d
Mohamed Soussi <mohameddsoussi@gmail.com>
2019-09-14 01:39:55 +0100

    use setCount from hooks in new functions

--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -7,13 +7,19 @@ const Head = () => {
     document.title = `You've clicked ${count} times`;
   });
 
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div>
       <p>
         You've clicked {count} times
       </p>
-      <button className="border rounded py-2 px-4 mt-2 ml-2 hover:bg-gray-100 hover:border-transparent" type="button" onClick={() => setCount(count + 1)}>
-        Click me
+      <button className="border rounded py-2 px-4 mt-2 ml-2 hover:bg-gray-100 hover:border-transparent" type="button" onClick={increment}>
+        +
+      </button>
+      <button className="border rounded py-2 px-4 mt-2 ml-2 hover:bg-gray-100 hover:border-transparent" type="button" onClick={decrement}>
+        -
       </button>
     </div>
   );
